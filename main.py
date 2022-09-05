a3d6852fae0b12804b04913441039a14349c2f22
Jonatas <jonatasfontele@gmail.com>
2022-09-05 18:04:22 -0300

    feat: sprite boundaries on the window

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pokemon import Pokemon
 # Initializing pygame and creating the window
 pygame.init()
 display = pygame.display.set_mode([840, 480])
-pygame.display.set_caption("Gather da Lampp-it")
+pygame.display.set_caption("Pokéball Invaders")
 
 #def draw():
     #display.fill([105, 61, 28])
@@ -19,15 +19,22 @@ haunter = Pokemon(object_group)
 
 # Music
 pygame.mixer.music.load("data/lavender-town.mp3")
+pygame.mixer.music.set_volume(0.05)
 pygame.mixer.music.play(-1)
 
 # Sounds
 cry = pygame.mixer.Sound("data/haunter.mp3")
+cry.set_volume(0.2)
 
 game_loop = True
 
+# For FPS
+clock = pygame.time.Clock()
+
 if __name__ == "__main__":
     while game_loop:
+        # 60 FPS
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_loop = False
@@ -35,10 +42,11 @@ if __name__ == "__main__":
                 if event.key == pygame.K_SPACE:
                     cry.play()
 
-        # draw()
-        display.fill([105, 61, 28])
-
+        # Update logic
         object_group.update()
+
+        # Draw
+        display.fill([105, 61, 28])
         object_group.draw(display)
 
         pygame.display.update()
--- pokemon.py
+++ pokemon.py
@@ -5,17 +5,29 @@ class Pokemon(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
 
-        self.image = pygame.image.load("data/Haunter.gif")
+        self.image = pygame.image.load("data/Haunter.png")
         self.image = pygame.transform.scale(self.image, [100, 100])
         self.rect = pygame.Rect(50, 50, 100, 100)
+
+        #self.speed = 0
+        #self.acceleration = 0.1
 
     def update(self, *args):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.rect.move_ip(-1, 0)
+            self.rect.move_ip(-5, 0)
         if keys[pygame.K_RIGHT]:
-            self.rect.move_ip(1, 0)
+            self.rect.move_ip(5, 0)
         if keys[pygame.K_UP]:
-            self.rect.move_ip(0, -1)
+            self.rect.move_ip(0, -5)
         if keys[pygame.K_DOWN]:
-            self.rect.move_ip(0, 1)
+            self.rect.move_ip(0, 5)
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > 480:
+            self.rect.bottom = 480
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > 840:
+            self.rect.right = 840
