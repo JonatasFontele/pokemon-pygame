f54ab0f01636a16bb5a911a90f92bc15bff3737f
Jonatas <jonatasfontele@gmail.com>
2022-09-30 11:24:03 -0300

    fix: all sprites coordinates from the sprite sheet

--- model/haunter.py
+++ model/haunter.py
@@ -15,43 +15,43 @@ class Haunter(model.pokemon.Pokemon):
         self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_idle2", 2))
         self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_idle3", 2))
 
-        # # Second row right — Forward
-        # self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward1", 2))
-        # self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward2", 2))
-        # self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward3", 2))
+        # Second row right — Forward
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward1", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward2", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_forward3", 2))
 
-        # # Third row right — Damage
-        # self.animation_list.append(self.image.get_image(480, 100, 55, 50, 2))
-        #
-        # # Fourth row right — Down
-        # self.animation_list.append(self.image.get_image(485, 142, 50, 57, 2))
-        # self.animation_list.append(self.image.get_image(425, 142, 60, 57, 2))
-        # self.animation_list.append(self.image.get_image(364, 142, 60, 57, 2))
-        # self.animation_list.append(self.image.get_image(295, 142, 65, 57, 2))
-        #
-        # # Fifth row right — Punch
-        # self.animation_list.append(self.image.get_image(470, 203, 65, 42, 2))
-        # self.animation_list.append(self.image.get_image(417, 203, 55, 42, 2)
-        # self.animation_list.append(self.image.get_image(368, 203, 50, 42, 2))
-        # self.animation_list.append(self.image.get_image(308, 203, 60, 42, 2))
-        # self.animation_list.append(self.image.get_image(238, 203, 70, 42, 2))
-        # self.animation_list.append(self.image.get_image(153, 203, 85, 42, 2)
-        #
-        # # Sixth row right — Lick
-        # self.animation_list.append(self.image.get_image(488, 248, 45, 62, 2))
-        # self.animation_list.append(self.image.get_image(425, 248, 60, 62, 2))
-        # self.animation_list.append(self.image.get_image(345, 248, 80, 62, 2))
-        # self.animation_list.append(self.image.get_image(265, 248, 80, 62, 2))
-        # self.animation_list.append(self.image.get_image(192, 248, 70, 62, 2))
-        # self.animation_list.append(self.image.get_image(125, 248, 65, 62, 2))
-        # self.animation_list.append(self.image.get_image(60, 248, 65, 62, 2)
+        # Third row right — Damage
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_damage", 2))
+
+        # Fourth row right — Down
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_down1", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_down2", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_down3", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_down4", 2))
+
+        # Fifth row right — Punch
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch1", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch2", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch3", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch4", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch5", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_punch6", 2))
+
+        # Sixth row right — Lick
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick1", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick2", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick3", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick4", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick5", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick6", 2))
+        self.animation_list.append(self.my_sprite_sheet.parse_sprite("haunter_lick7", 2))
 
         # First row right — Shiny
         # animation_list.append(sprite_sheet.get_image(0, 0, 65, 45, 2))
 
         # Animation
         self.last_update = pygame.time.get_ticks()
-        self.animation_cooldown = 400
+        self.animation_cooldown = 200
         self.frame = 0
         self.animation = False
         self.image = self.animation_list[self.frame - 1]
